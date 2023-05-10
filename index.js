180295e4d046d2c72a74a91f0684c0b90080f7ff
nduking <114291880+nduking@users.noreply.github.com>
2023-05-10 15:19:25 +0100

    update

---
+++ index.js
@@ -0,0 +1,12 @@
+// const firstName = document.getElementById("firstno").value;
+// const secondName = document.getElementById("secondno").value;
+
+function calc(event) {
+      const firstName = document.getElementById("firstno").value;
+      const secondName = document.getElementById("secondno").value;
+      let result = firstno + secondno;
+      console.log(result);
+
+let result = firstno ** secondno;
+}
+button type="submit" onclick="calc(event)"> submit</button
