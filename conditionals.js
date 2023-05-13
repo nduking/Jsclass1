4b46b9e7a8109c40d0907a574eaffe89ec401913
nduking <114291880+nduking@users.noreply.github.com>
2023-05-13 13:17:58 +0100

    conditionals

---
+++ conditionals.js
@@ -0,0 +1,16 @@
+let isRaining = false
+
+if (isRaining) {
+      console.log("Bring a rain coat");
+} else {
+      console.log ("No need for a rain coat");
+}
+
+let age = 18
+if(age === 18){
+      console.log("can watch")
+} else if (age > 18){
+            console.log("More than enough")
+} else {
+console.log("get out")
+}
