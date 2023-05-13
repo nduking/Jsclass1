2e0da493c19edc4cc1e294702969b270c4c8a202
nduking <114291880+nduking@users.noreply.github.com>
2023-05-13 20:37:51 +0100

    operator assignment

---
+++ operatorSolution.js
@@ -0,0 +1,15 @@
+let firstName ="Nduka";
+let lastName = "Kingsley";
+let country = "Nigeria";
+let city = "Lagos";
+let age = 21;
+let isMarried = true;
+let year = 2023;
+console.log(typeof lastName);
+console.log(typeof age);
+console.log(isMarried);
+console.log('10'==10);
+console.log('10'===10);
+console.log(parseInt(9.8)===10);
+console.log("Nduka".length === "kingsley".length);
+console.log("Nduka".length !== "kingsley".length);
