04a4f861f0f2528673c05ef69c4bae8349b3be0b
nduking <114291880+nduking@users.noreply.github.com>
2023-05-14 13:13:29 +0100

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,18 @@
+// const className = ["Kelechi","Kelvin","Justina","Evans","Dolapo","Evans","Ifeanyi","Lanre","Isreal"];
+// console.log(className);
+const className = [];
+className.push("Kelechi","Ifeanyi","Dolapo","Kelvin","Justina","Evans","Folarine","Isreal","Nduka");
+console.log(className);
+className.push("Julius");
+console.log(className);
+className.pop("Julius");
+console.log(className);
+className.pop("Nduka");
+console.log(className);
+className.pop();
+console.log(className);
+className.shift();
+console.log(className);
+className.unshift("Julius");
+console.log(className);
+
