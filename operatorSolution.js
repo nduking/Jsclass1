9e7c7db356fb81143ebf8d3b53e1e443765ddc20
nduking <114291880+nduking@users.noreply.github.com>
2023-05-14 11:27:36 +0100

    update

--- operatorSolution.js
+++ operatorSolution.js
@@ -7,7 +7,7 @@ let isMarried = true;
 let year = 2023;
 console.log(typeof lastName);
 console.log(typeof age);
-console.log(isMarried);
+console.log(`${isMarried}`);
 console.log('10'==10);
 console.log('10'===10);
 console.log(parseInt(9.8)===10);
