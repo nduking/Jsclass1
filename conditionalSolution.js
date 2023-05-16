05c6e8e9a647f0696ae842f275bb36bfc41f742b
nduking <114291880+nduking@users.noreply.github.com>
2023-05-16 17:06:37 +0100

    updated assignment

---
+++ conditionalSolution.js
@@ -0,0 +1,8 @@
+let age = prompt;
+if (age >= 18){
+      console.log(`Your age is ${age}, you are old enough to drive`);
+} else if (age === 15){
+      console.log(`Your age is ${age},you are still have 3 years left to drive `);
+}else {
+      console.log('Welldone');
+}
